e0233371ec6c3d8239263e327b87c246a9c813de
Théo Bouveyron <44236116+Demirro@users.noreply.github.com>
2020-11-24 13:49:06 +0100

    Add files via upload

---
+++ src/de/uk/java/Application.java
@@ -0,0 +1,26 @@
+package de.uk.java;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// Neues Konto mit vorgegebenen Kontostand
+		Konto konto1 = new Konto(1, "Mustermann", 30);
+		// Neues Konto ohne vorgegebenen Kontostand
+		Konto konto2 = new Konto(2, "Musterfrau");
+		
+		// In das erste Konto wird erst eingezahlt und dann ausgezahlt
+		konto1.einzahlen(1);
+		konto1.auszahlen(20);
+		
+		// Hier sollte eine Fehlermeldung kommen, da das Konto nicht genug gedeckt ist. 
+		// Die Fehlermeldung kommt aus der Methode
+		konto1.auszahlen(200);
+		
+		// Getter testen
+		konto2.getKontostand();
+		konto2.getId();
+		konto2.getName();
+	}
+
+}
---
+++ src/de/uk/java/Konto.java
@@ -0,0 +1,23 @@
+package de.uk.java;
+
+public class Konto {
+	// Instanzvariablen initiieren
+	private int id;
+	private String name;
+	private int kontostand;
+	
+	// Konstruktor nimmt id und name an
+	
+	// Konstruktor nimmt id, name und kontostand an
+	
+	// getId
+
+	// getName
+	
+	// getKontostand
+	
+	// Methode einzahlen - Eine Methode um dem Kontostand einen bestimten Wert hinzuzufügen
+	
+	// Methode auszahlen - eine Methode die eine bestimmte Menge von dem Konto abzieht, sofern genug Geld auf dem Konto ist
+	
+}
